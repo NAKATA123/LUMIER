1d11ae6ffc0054dddda239cf1f91f42cb076fabc
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-04-26 21:10:23 +0900

    変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,21 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbo:load", () => {
   const btn = document.getElementById("menu-toggle");
-  const nav = document.getElementById("nav");
+  const nav = document.getElementById("mobile-nav");
+  const overlay = document.getElementById("overlay");
 
   if (!btn || !nav) return;
 
   btn.addEventListener("click", () => {
     nav.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+
+  // 背景クリックで閉じる
+  overlay.addEventListener("click", () => {
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
   });
 });
