d579fb15072dfc8c947066ab3217878a11eea429
RALPH <ralph@RALPHnoMacBook-Air.local>
2026-04-26 09:36:58 +0900

    price.about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  # aboutページ（ルミエールのご紹介）
+  get "about", to: "home#about"
+
+  #料金ページ
+  get "price", to: "home#price"
+
   # 個人情報ページ
   get "privacy", to: "static#privacy"
 
